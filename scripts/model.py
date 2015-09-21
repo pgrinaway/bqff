ecbf58ad0c6b00792bc09fe166794c2366e3b1b2
pgrinaway <win98pro>
2015-09-21 15:21:53 -0400

    initial commit

---
+++ scripts/model.py
@@ -0,0 +1,68 @@
+
+
+__author__ = 'Patrick B. Grinaway'
+
+import scipy.stats as stats
+import energies
+
+class GBFFmodel(object):
+    """
+    This class contains the implementation for the GBFF model, without using PyMC
+
+    Arguments
+    ---------
+    prepared_database : dict
+        A prepared FreeSolv database
+    """
+
+    def __init__(self, prepared_database, initial_parameters):
+        self._database = prepared_database
+        self._initial_parameters = initial_parameters
+
+
+    def ln_prior(self, parameters):
+        """
+        Calculate the log prior probability of the FF parameters
+        """
+        lnprior = 0.0
+        for (key, value) in parameters.iteritems():
+            parmtype = key.split("_")[1]
+            if parmtype == 'radius':
+                lnprior += stats.distributions.uniform.logpdf(value, 0.5, 2.5)
+            elif parmtype == 'scalingFactor':
+                lnprior += stats.distributions.uniform.logpdf(value, -0.8, 1.5)
+            elif parmtype == 'sigma':
+                lnprior += stats.distributions.invgamma.logpdf(value,1.0,1.0)
+        return lnprior
+
+
+    def ln_likelihood(self, parameters):
+        """
+        This calculates the log-likelihood of the given parameters.
+
+        Arguments
+        ---------
+        parameters : dict
+            A dictionary of the current set of parameters
+
+        Returns
+        -------
+        ln_like : float
+            The log-likelihood of the parameters
+        """
+        ln_like = 0.0
+
+        #calculate free energies--returns a dictionary of {cid : DeltaG}
+        delta_g = energies.compute_hydration_energies_sequentially(self._database, parameters)
+
+        #iterate through and add each molecule's contribution to the loglikelihood
+        for cid in delta_g.iterkeys():
+            dg_gbsa = delta_g[cid]
+            dg_exp = self._database[cid]['expt']
+            ddg_expt = self._database[cid]['d_expt']
+            normal_sigma = parameters['model_sigma']**2 + ddg_expt **2
+            print('The calculated dG for %s is %s, and the experimental is %f' % (self._database[cid]['iupac'], str(dg_gbsa), dg_exp))
+            ln_like += stats.distributions.norm.logpdf(dg_gbsa, dg_exp, normal_sigma)
+
+        return ln_like
+
---
+++ scripts/parameterize_obc.py
@@ -0,0 +1,205 @@
+__author__ = 'Patrick B. Grinaway'
+
+"""
+This script will use Bayesian optimization to parameterize an OBC forcefield. Options are specified in a yaml file
+"""
+
+import GPy
+import GPyOpt
+
+import simtk.openmm as openmm
+import simtk.openmm.app as app
+import simtk.unit as units
+
+import numpy as np
+import yaml
+import pickle
+
+
+
+
+def _get_parameter_bounds(list_of_parameter_names):
+    """
+    Gets a list of parameter bounds (alphabetical) from the parameter names
+
+    Arguments
+    ---------
+    list_of_parameter_names : list of string
+       names of parameters
+
+    Returns
+    -------
+    bounds : list of tuples
+        Bounds for the parameters of interest
+    """
+    list_of_parameter_names.sort()
+    bounds = []
+    for name in list_of_parameter_names:
+        parmtype = name.split('_')[1]
+        if parmtype == 'radius':
+            bounds.append((0.5, 2.5))
+        elif parmtype == 'scalingFactor':
+            bounds.append((-0.8, 1.5))
+        elif parmtype == 'sigma':
+            bounds.append((0, 1000))
+        else:
+            raise ValueError
+    return bounds
+
+def read_gbsa_parameters(filename):
+        """
+        Read a GBSA parameter set from a file.
+
+        ARGUMENTS
+
+        filename (string) - the filename to read parameters from
+
+        RETURNS
+
+        parameters (dict) - parameters[(atomtype,parameter_name)] contains the dimensionless parameter
+
+        TODO
+
+        * Replace this with a standard format?
+
+        """
+
+        parameters = dict()
+
+        infile = open(filename, 'r')
+        for line in infile:
+            # Strip trailing comments
+            index = line.find('%')
+            if index != -1:
+                line = line[0:index]
+
+            # Parse parameters
+            elements = line.split()
+            print("the length of the elements is %d" % len(elements))
+            if len(elements) == 3:
+                [atomtype, radius, scalingFactor] = elements
+                parameters['%s_%s' % (atomtype,'radius')] = float(radius)
+                parameters['%s_%s' % (atomtype,'scalingFactor')] = float(scalingFactor)
+            elif len(elements) == 6:
+                [atomtype, radius, scalingFactor, alpha, beta, gamma] = elements
+                parameters['%s_%s' % (atomtype,'radius')] = float(radius)
+                parameters['%s_%s' % (atomtype,'scalingFactor')] = float(scalingFactor)
+                parameters['%s_%s' % (atomtype,'alpha')] = float(alpha)
+                parameters['%s_%s' % (atomtype,'beta')] = float(beta)
+                parameters['%s_%s' % (atomtype,'gamma')] = float(gamma)
+        return parameters
+
+def _param_list_to_dict(parameter_list, list_of_param_names):
+    """
+    Convert the list of parameters to a dictionary for the model
+
+    Arguments
+    ---------
+    parameter_list : list of floats
+        The current set of parameters that GPyOpt wants to try
+    list_of_param_names : list of string
+        The list of the names of the parameters.
+
+    Returns
+    -------
+    parameter_dict_list : dict
+        A dictionary of the format {parameter_name : parameter_value}
+
+    """
+
+    list_of_param_names.sort()
+    parameter_dict = {}
+    for (i, val) in enumerate(parameter_list):
+        parameter_dict[list_of_param_names[i]] = val
+    return parameter_dict
+
+
+def obc_model_likelihood(parameters, model=None, list_of_param_names=None):
+    """
+    This is a function that is callable by GPyOpt. Specifically, it
+    takes in a list of parameters (alphabetical order--GPyOpt uses lists instead of dicts)
+    and creates a dict with the appropriate parameters for the model.
+
+    Arguments
+    ---------
+    parameters : list of float
+        The current set of parameters that GPyOpt wants to try
+    model : GBFFModel
+        An instantiated GBFFmodel that can calculate the log prior and likelihood
+
+    Returns
+    -------
+    ln_like : float
+        The log likelihood of the model
+    """
+    ln_unnormalized = []
+    for parameter_set in parameters:
+        parameter_dict = _param_list_to_dict(parameter_set, list_of_param_names)
+        lnprior = model.ln_prior(parameter_dict)
+        lnlikelihood = model.ln_likelihood(parameter_dict)
+        ln_unnormalized.append([lnprior+lnlikelihood])
+    print(ln_unnormalized)
+    return np.array(ln_unnormalized)
+
+def gpy_f_factory(model, list_of_param_names):
+    """
+    returns a function callable by GPyOpt
+    """
+    def gpy_f(*parameters):
+        return obc_model_likelihood(*parameters, model=model, list_of_param_names=list_of_param_names)
+    return gpy_f
+
+if __name__ == "__main__":
+    import os
+    import model
+
+    #read in configurations
+    yamlfile = open('configs.yaml','r')
+    configs = yaml.load(yamlfile)
+    yamlfile.close()
+
+    prepared_database_file = configs['database_file']
+    parameter_file = configs['parameter_file']
+    max_iter = configs['max_iter']
+    n_molecules = configs['n_molecules']
+
+    print(os.getcwd())
+
+    #read in the parameters
+    parameters = read_gbsa_parameters(parameter_file)
+    parameters['model_sigma'] = 100
+    parameter_names = parameters.keys()
+
+    #get initial set of parameters in list form:
+    parameter_names.sort()
+    initial_parameter_list = []
+    for (i, name) in enumerate(parameter_names):
+        initial_parameter_list.append(parameters[name])
+    initial_parameter_array = np.array([initial_parameter_list])
+
+    #load the database
+    db_file = open(prepared_database_file,'r')
+    database = pickle.load(db_file)
+    db_file.close()
+
+    #create a subset for debugging:
+    subset_size = n_molecules
+    cid_list = database.keys()
+    max_num = len(cid_list)
+    mol_indices = np.random.choice(max_num, subset_size)
+    mols_to_use = [cid_list[k] for k in mol_indices]
+    database = dict((k, database[k]) for k in mols_to_use)
+
+    #instantiate the model
+    model = model.GBFFmodel(database, parameters)
+
+    #generate the bounds for each variable
+    bounds = _get_parameter_bounds(parameter_names)
+
+    #generate the GPyOpt callable
+    likelihood = gpy_f_factory(model, parameter_names)
+
+    #get GPyOpt started
+    gpyopt = GPyOpt.methods.BayesianOptimization(likelihood, bounds, X=initial_parameter_array)
+
+    gpyopt.run_optimization(max_iter)
